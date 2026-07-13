1687b7489bea9301c4b65f57bb65fd53ced290bc
Tu Nombre <tu-email@ejemplo.com>
2026-07-12 21:16:19 -0400

    Change checkbox text to 'Para información de precios contacte al vendedor'

--- admin/index.php
+++ admin/index.php
@@ -482,7 +482,7 @@ if (isset($_GET['logout'])) {
                 <div class="form-group form-group-checkbox">
                     <label>
                         <input type="checkbox" name="precio_a_convenir" id="f_precio_a_convenir" value="1" onchange="togglePrecio()">
-                        Para más información contacte al vendedor
+                        Para información de precios contacte al vendedor
                     </label>
                 </div>
                 <div class="form-group">
